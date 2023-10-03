695cd063c08269f8c5fdf038e7995198db8066d1
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-03 16:00:19 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+// import { Routes, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+
+// import Home from 'path/to/pages/Home';
+// import About from 'path/to/pages/About';
+// import Products from 'path/to/pages/Products';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <button
+        style={{
+          display: 'flex',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        HOME / START
+      </button>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} /> */}
+      </Routes>
     </div>
   );
 };
