8fe43218710847182fbf640173e172b5e02f1421
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-04 16:23:52 +0200

    header

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -4,8 +4,10 @@ export const HeaderStyled = styled('header')({
   position: 'fixed',
   zIndex: '20',
   width: '100%',
-  // background: 'whitesmoke',
-  boxShadow: '0 4px 8px 2px #0a0a0a',
+  // background: 'teal',
+  background: 'rgba(0, 128, 128, 0.6)',
+
+  boxShadow: '0px 4px 10px 2px rgba(0, 0, 0, 0.5)',
 
   '&>div': {
     display: 'flex',
