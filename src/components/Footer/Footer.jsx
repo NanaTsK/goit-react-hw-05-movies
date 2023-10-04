9a12ecacbacd2088f9eb929eae5975ab9bfad638
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-04 15:23:35 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 // import Logo from 'components/Logo';
 // import { FooterStyled } from './Footer.styled';
 import { FooterStyled } from './Footer.styled';
+import { Container } from 'components/App.styled';
 
 export const Footer = () => {
   return (
     <FooterStyled>
-      {/* <Logo /> */}
-      <button>FOOTER LOGO</button>
+      <Container>
+        {/* <Logo /> */}
+        <button>FOOTER LOGO</button>
+      </Container>
     </FooterStyled>
   );
 };
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,13 +1,24 @@
 import styled from 'styled-components';
 
 export const FooterStyled = styled('footer')({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  right: 0,
 
-  minHeight: '100px',
-  //   background: '#1f1f1f1f',
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+
+  //   minHeight: '100px',
   background: 'teal',
 
   boxShadow: '0 -4px 8px 2px #0a0a0a',
+
+  '&>div': {
+    display: 'flex',
+    //   display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
