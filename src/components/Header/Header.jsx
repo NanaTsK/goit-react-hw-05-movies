1f1edd672c77740a7faf8f6406b65f9e15c46074
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-05 17:32:56 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,12 @@
 import { HeaderStyled } from './Header.styled';
 import { Container } from 'components/App.styled';
 import { NavList } from 'components/NavList/NavList';
-// import Logo from 'components/Logo';
 
 export const Header = () => {
   return (
     <HeaderStyled>
       <Container>
-        {/* <button>HEADER</button> */}
         <NavList />
-        {/* <Logo /> */}
       </Container>
     </HeaderStyled>
   );
