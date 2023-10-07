dd6fc91e3847ddebd519d1d8c52c6218c3182b29
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-07 19:35:00 +0200

    footer

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -2,21 +2,6 @@ import styled from 'styled-components';
 
 const transition = '250ms cubic-bezier(0.4, 0, 0.2, 1)';
 
-// export const FooterStyled = styled('footer')({
-//   position: 'fixed',
-// bottom: 0,
-// left: 0,
-// right: 0,
-
-//   background: 'rgba(0, 128, 128, 0.6)',
-//   boxShadow: '0px -4px 10px 2px rgba(0, 0, 0, 0.5)',
-
-//   '&>div': {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
 export const FooterStyled = styled('footer')({
   position: 'fixed',
   bottom: 0,
