7eb146eeacd03e2ed766b563cb49246500b092d3
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-07 18:31:17 +0200

    MoviesList

--- src/components/MoviesItem/MoviesItem.jsx
+++ src/components/MoviesItem/MoviesItem.jsx
@@ -1,7 +1,6 @@
 import { useLocation, Link } from 'react-router-dom';
 import defaultImg from 'images/defImg.jpg';
 import { MoviesItemStyled, TitleWrap, MovieTitle } from './MoviesItem.styled';
-// import { MoviesItemStyled, TitleWrap, MovieTitle } from './MoviesItem.styled';
 
 export const MoviesItem = ({ movie }) => {
   const location = useLocation();
