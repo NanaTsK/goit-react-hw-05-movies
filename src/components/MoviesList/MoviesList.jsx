547c1358812240bc1a21d5fab5b4d42e6d9dca63
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-07 18:15:11 +0200

    MoviesList

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,5 +1,6 @@
 import { MoviesItem } from 'components/MoviesItem/MoviesItem';
 import { MoviesListStyled } from './MoviesList.styled';
+// import { MoviesListStyled } from './MoviesList.styled';
 
 export const MoviesList = ({ moviesList }) => {
   return (
